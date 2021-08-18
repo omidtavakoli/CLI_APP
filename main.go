f2d54403e6f418bbaef84c734429f1ac26d6e36a
Omid Tavakoli <omidtavakoli.info@gmail.com>
2021-08-18 11:15:27 +0430

    data cleaning added

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"github.com/gocarina/gocsv"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 	"log"
 	"os"
+	"sync"
 )
 
 var app = cli.NewApp()
@@ -48,20 +50,138 @@ func commands() {
 	}
 }
 
-func Calculation() error {
-	//todo: load data to memory
-	logrus.Warn("implement me : calculation")
+func Find(slice []string, val string) (int, bool) {
+	for i, item := range slice {
+		if item == val {
+			return i, true
+		}
+	}
+	return -1, false
+}
+
+func loadCSV(filePath string, out interface{}) error {
+	file, err := os.OpenFile(filePath, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	if err := gocsv.UnmarshalFile(file, out); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func fetchData() ([]*Repo, []*Commit, []*Actor, []*Event, error) {
+	var repos []*Repo
+	err := loadCSV("./data/repos.csv", &repos)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+
+	var commits []*Commit
+	err = loadCSV("./data/commits.csv", &commits)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+
+	var actors []*Actor
+	err = loadCSV("./data/actors.csv", &actors)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+
+	var events []*Event
+	err = loadCSV("./data/events.csv", &events)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+
+	return repos, commits, actors, events, nil
+}
+
+func cleanData(events []*Event, commits []*Commit, repos []*Repo, actors []*Actor) {
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		for _, event := range events {
+			wg2 := sync.WaitGroup{}
+			wg2.Add(2)
+			go func() {
+				defer wg2.Done()
+				var commitsList []string
+				var uniqueCommits []*Commit
+				for _, commit := range commits {
+					_, find := Find(commitsList, commit.SHA)
+					if !find {
+						if commit.EventID == event.ID {
+							event.Message = commit.Message
+							event.SHA = commit.SHA
+						}
+						commitsList = append(commitsList, commit.SHA)
+						uniqueCommits = append(uniqueCommits, commit)
+					}
+				}
+				commits = uniqueCommits
+			}()
+			go func() {
+				defer wg2.Done()
+				var reposList []string
+				var uniqueRepos []*Repo
+				for _, repo := range repos {
+					_, find := Find(reposList, repo.Name)
+					if !find {
+						if repo.ID == event.RepoID {
+							event.Repo = repo
+						}
+						reposList = append(reposList, repo.Name)
+						uniqueRepos = append(uniqueRepos, repo)
+					}
+				}
+			}()
+			wg2.Wait()
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		//used for ignoring duplicates
+		var usersList []string
+		var uniqueActors []*Actor
+		for _, user := range actors {
+			_, find := Find(usersList, user.UserName)
+			if !find {
+				for _, event := range events {
+					if event.ActorID == user.ID {
+						user.Events = append(user.Events, event)
+					}
+				}
+				usersList = append(usersList, user.UserName)
+				uniqueActors = append(uniqueActors, user)
+			}
+		}
+		actors = uniqueActors
+	}()
+
+	wg.Wait()
 }
 
 func main() {
 	info()
-	commands()
-	err := Calculation()
+	repos, commits, actors, events, err := fetchData()
 	if err != nil {
-		logrus.Error(err)
-		panic("loading data error")
+		panic(err.Error())
 	}
+
+	cleanData(events, commits, repos, actors)
+
+	logrus.Info("len: ", len(repos), len(commits), len(actors), len(events))
+
+	commands()
 	err = app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
--- model.go
+++ model.go
@@ -1,24 +1,32 @@
 package main
 
-type User struct {
-	ID       int64
-	UserName string
+type Actor struct {
+	ID       int64  `csv:"id"`
+	UserName string `csv:"username"`
+	Events   []*Event
+	Activity int
 }
 
-type Commit struct {
+// Event : consider commit as an event
+type Event struct {
+	ID      int64  `csv:"id"`
+	Type    string `csv:"type"`
+	ActorID int64  `csv:"actor_id"`
+	RepoID  int64  `csv:"repo_id"`
 	SHA     string
 	Message string
-	EventID int64
-}
-
-type Events struct {
-	ID     int64
-	Type   string
-	UserID int64
-	RepoID int64
+	Repo    *Repo
 }
 
 type Repo struct {
-	ID   int64
-	Name string
+	ID           int64  `csv:"id"`
+	Name         string `csv:"name"`
+	CommitsCount int64
+	WatchesCount int64
+}
+
+type Commit struct {
+	SHA     string `csv:"sha"`
+	Message string `csv:"message"`
+	EventID int64  `csv:"event_id"`
 }
